c075c82a1caeb460ed9cab2dc03c8ee9971170cc
Dirk Kok <kok1991@gmail.com>
2015-07-05 02:07:22 +0200

    Update script

--- Arduino-ESP8266-TMP36-IoT-Temperature-Sensor.ino
+++ Arduino-ESP8266-TMP36-IoT-Temperature-Sensor.ino
@@ -7,75 +7,72 @@ SoftwareSerial mySerial(10, 11);
 
 void setup() {
   Serial.begin(9600); // Connection to PC
-  mySerial.begin(9600); // Connection to ESP8266  
+  mySerial.begin(9600); // Connection to ESP8266
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
-  
+
   pinMode(7, OUTPUT);
   digitalWrite(7, LOW);
 }
 
 void loop() {
-//  Data url: https://data.sparkfun.com/isen_arduino_esp8266_demo
-
   float temp = getTemperatureAverage();
   Serial.println("Temperature: " + String(temp));
   sendTemperature(temp);
-  
+
   delay(10000);
 }
 
 void sendTemperature(float temperature) {
   digitalWrite(7, HIGH);
-  delay(2000);  
-  
+  delay(2000);
+
   mySerial.println("AT+RST");
   WaitForReady(2000);
   mySerial.println("AT+CWMODE=1");
   WaitForOK(2000);
   mySerial.println("AT+RST");
   WaitForReady(2000);
-  
-  mySerial.println("AT+CWJAP=\"Dirks Draadloos\",\"DirkUit1991\"");
+
+  mySerial.println("AT+CWJAP=\"<wifi_ssid>\",\"<wifi_password>\"");
   if (WaitForOK(5000)) {
     digitalWrite(13, HIGH); // Connection succesful
   }
-  
+
   mySerial.println("AT+CIPSTART=\"TCP\",\"data.sparkfun.com\",80");
   WaitForOK(5000);
   mySerial.println("AT+CIPSEND=123");
   WaitForOK(5000);
-  mySerial.print("GET /input/zD0AyAo84RiNb1dMLXzw?private_key=Yy6lKlxkVetv8o6bpJEZ&temp=" + String(temperature) + " HTTP/1.0\r\n");
+  mySerial.print("GET /input/<public_key>?private_key=<private_key>&temp=" + String(temperature) + " HTTP/1.0\r\n");
   mySerial.print("Hostname: data.sparkfun.com\r\n\r\n");
   WaitForOK(5000);
   mySerial.println("AT+CIPCLOSE");
   WaitForOK(5000);
-  
+
   digitalWrite(13, LOW);
   digitalWrite(7, LOW);
-  
 }
 
 float getTemperatureAverage() {
   Average<float> ave(10);
-  for(int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     ave.push(getTemperature());
     delay(500);
   }
-  
+
   float total = 0.0;
   delay(50);
-  
+
   float temperature = ave.mean();
-  
+
   return temperature;
 }
 
 float getTemperature() {
   int sensorVal = analogRead(A0);
-  float voltage = (sensorVal/1024.0) * 5.0;
+  float voltage = (sensorVal / 1024.0) * 5.0;
   float temperature = (voltage - .5) * 100;
-  
+
   return temperature;
 }
 
@@ -90,18 +87,18 @@ boolean WaitForReady(long timeoutamount) {
 // Parts used from https://github.com/contractorwolf/ESP8266
 boolean WaitForResponse(String response, long timeoutamount) {
   unsigned long timeout = millis() + timeoutamount;
-  
+
   while (millis() <= timeout) {
     while (mySerial.available() > 0) {
       int len = mySerial.readBytesUntil('\n', serialbuffer, sizeof(serialbuffer));
-    
-      String message = String(serialbuffer).substring(0,len-1);
-       
+
+      String message = String(serialbuffer).substring(0, len - 1);
+
       if (message == response) {
         return true;
       }
     }
   }
-  
+
   return false;
 }
